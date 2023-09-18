cf177ac15074c4217d52c26ed94f08d216822a46
MrAntunes09 <victorhugoantunes2006@gmail.com>
2023-09-18 14:03:44 -0300

    criação index e arquivo css

--- TCCC/Site_BootStrap/index.php
+++ TCCC/Site_BootStrap/index.php
@@ -1,92 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-
 <?php
-  require_once 'head.php';
+    require_once 'head.php'; // Importe o código do header e as estilizações
 ?>
 
-<body>
-
-  <section id="nav">
-   <?php require_once 'nav.php'; ?>
-  </section>
-
-  <section id="content" class="bk-fundo">
-    <div class="container bk-fundo-container">
-
-      <div class="img-inicio">
-        <img src="img/img1.png" class="img-fluid" alt="">
-      </div>
-      
-      <!--CARDS-->
-<br>
-      <div class="row row-cols-1 row-cols-md-3 g-4">
-        <div class="col">
-          <div class="card h-100">
-            <img src="img/logo.png" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
-                content. This content is a little bit longer.</p>
+<!-- Conteúdo principal do seu site -->
+<div class="img-container">
+    <img src="img/img1fullclean.png" class="img-fluid" alt="">
+    <div class="overlay">
+        <div class="conteudo">
+            <div class="nome">Buscachorro</div>
+            <div class="descricao">Porque cada rabo abanando merece um final feliz.</div>
+            <div class="botoes">
+                <a href="#" class="botao">Entrar</a>
+                <a href="#" class="botao">Cadastrar</a>
             </div>
-          </div>
         </div>
-        <div class="col">
-          <div class="card h-100">
-            <img src="img/logo.png" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a short card.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card h-100">
-            <img src="img/logo.png" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
-                content.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card h-100">
-            <img src="img/logo.png" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
-                content. This content is a little bit longer.</p>
-            </div>
-          </div>
-        </div>
-
-        <div class="col">
-          <div class="card h-100">
-            <img src="img/logo.png" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
-                content. This content is a little bit longer.</p>
-            </div>
-          </div>
-        </div>
-
-        <div class="col">
-          <div class="card h-100">
-            <img src="img/logo.png" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
-                content. This content is a little bit longer.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-
     </div>
-
-  </section>
-</body>
-
-</html>
+</div>
