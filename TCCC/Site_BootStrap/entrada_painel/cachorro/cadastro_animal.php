b9773c16540ffccb6eb23c40cd32929d63dbcead
PedroRosaEtec <pedro.rosa37@etec.sp.gov.br>
2023-09-27 21:48:08 -0300

    implantação do Cad de fotos no cadastro de animais

--- TCCC/Site_BootStrap/entrada_painel/cachorro/cadastro_animal.php
+++ TCCC/Site_BootStrap/entrada_painel/cachorro/cadastro_animal.php
@@ -1,3 +1,47 @@
+
+<?php
+
+strtolower(pathinfo("arquivo.jpg", PATHINFO_EXTENSION));
+
+if(!empty($_FILES['arquivo'])){
+    $arquivo = $_FILES['arquivo'];
+
+    if($arquivo['error']){
+        die("Falha ao enviar o arquivo");
+    }
+    if($arquivo['size'] > 2097152){
+        die("Arquivo muito grande! Tamanho maximo permitido: 2MB!");
+    }
+
+    $diretorio = "fotos_animais/";
+    $nome_arquivo = $arquivo['name'];
+    $novo_nome_arquivo = uniqid();
+    $extensao_arquivo = strtolower(pathinfo($nome_arquivo, PATHINFO_EXTENSION));
+
+    if($extensao_arquivo != "jpg" && $extensao_arquivo != "png"){
+        die("Extensão não permitida!");
+    }
+
+    $upload_true = move_uploaded_file($arquivo['tmp_name'], $diretorio . $novo_nome_arquivo . "." . $extensao_arquivo);
+
+    $nome_com_extensao =   $novo_nome_arquivo . "." . $extensao_arquivo;
+
+    if($upload_true){
+        //echo "Upload efetuado com sucesso! <a target = '_blanck' href='fotos_animais/$nome_com_extensao'>Clique aqui para acessa-lo!</a>";
+
+        $sql = '
+        INSERT INTO tb_foto set
+        url_imagem = "'.$nome_com_extensao.'",
+        
+        '
+    }
+    else{
+        echo "Deu ruim!";
+    }
+
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,61 +50,10 @@
 <body>
     <h1>Cadastro de Cachorro</h1>
     
-    <form action="#" method="post" enctype="multipart/form-data">
-        <label for="foto">Foto do Cachorro:</label>
-        <input type="file" id="foto" name="foto" accept="image/*">
-        <br>
-
-        <label for="nome">Nome do Cachorro:</label>
-        <input type="text" id="nome" name="nome" required>
-        <br>
-
-        <label for="raca">Raça:</label>
-        <input type="text" id="raca" name="raca" required>
-        <br>
-
-        <label for="idade">Idade:</label>
-        <input type="number" id="idade" name="idade" required>
-        <br>
-
-        <label for="descricao">Descrição:</label>
-        <textarea id="descricao" name="descricao" rows="4" required></textarea>
-        <br>
-
-        <input type="submit" value="Cadastrar">
-    </form>
-</body>
-</html>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Cadastro de Cachorro</title>
-</head>
-<body>
-    <h1>Cadastro de Cachorro</h1>
-    
-    <form action="#" method="post" enctype="multipart/form-data">
-        <label for="foto">Foto do Cachorro:</label>
-        <input type="file" id="foto" name="foto" accept="image/*">
-        <br>
-
-        <label for="nome">Nome do Cachorro:</label>
-        <input type="text" id="nome" name="nome" required>
-        <br>
-
-        <label for="raca">Raça:</label>
-        <input type="text" id="raca" name="raca" required>
-        <br>
-
-        <label for="idade">Idade:</label>
-        <input type="number" id="idade" name="idade" required>
-        <br>
-
-        <label for="descricao">Descrição:</label>
-        <textarea id="descricao" name="descricao" rows="4" required></textarea>
-        <br>
-
-        <input type="submit" value="Cadastrar">
+    <form action="" method="post" enctype="multipart/form-data">
+        <p><label for="">Selecione o arquivo que deseja enviar!</label></p>
+        <input type="file" name="arquivo">
+        <input type="submit" value="Cadastrar" name="upload">
     </form>
 </body>
 </html>
