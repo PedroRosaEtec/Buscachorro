08acceee351d5ac5463e23d7d0ec60328c9613a3
PedroRosaEtec <pedro.rosa37@etec.sp.gov.br>
2023-09-27 22:23:39 -0300

    a

--- TCCC/Site_BootStrap/entrada_painel/head.php
+++ TCCC/Site_BootStrap/entrada_painel/head.php
@@ -12,4 +12,4 @@
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 
-</head>
+</head>a
