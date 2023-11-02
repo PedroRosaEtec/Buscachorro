67499e443a1672c6e684b0a519a9f3ef2f1aec94
PedroRosaEtec <133720869+PedroRosaEtec@users.noreply.github.com>
2023-11-02 00:40:54 -0300

    Add files via upload

---
+++ newtcc/conect.php
@@ -0,0 +1,10 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$database = "db_buscachorro";
+
+$con = new mysqli($host, $user, $pass, $database);
+
+?>
---
+++ newtcc/painel-cliente/navigation.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="index.php">
+    <img src="../img/logo.png" alt="" width="50" height="50">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav ml-auto"> <!-- Adicione ml-auto aqui -->
+      <li class="nav-item active">
+        <a class="nav-link" href="cadastro_animal.php">Cadastrar Animal</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="painel_controle.php">Configurações</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="../logoff.php">Deslogar</a>
+      </li>
+    </ul>
+  </div>
+</nav>
