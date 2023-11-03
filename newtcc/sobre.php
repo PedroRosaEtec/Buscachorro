8a7e2dc86bd704b9d648c874bac4fd75fb0bbc41
Goulartgs <139942513+Goulartgs@users.noreply.github.com>
2023-11-03 16:54:27 -0300

    erro index
    tirando o card do sobre

--- newtcc/sobre.php
+++ newtcc/sobre.php
@@ -30,43 +30,4 @@ require_once "navigation.php";
         </div>
     </div>
 
-
-    <div class="container">
-        <div class="row">
-            <?php
-            $sql = '
-        SELECT nm_animal, descricao, url_imagem
-        FROM tb_animal AS an
-        INNER JOIN tb_foto AS ft ON an.cd_animal = ft.id_animal;
-        ';
-
-            $res = $GLOBALS['con']->query($sql);
-
-            while ($dados = mysqli_fetch_assoc($res)) {
-                ?>
-
-                <div class="col-md-3 mb-3">
-                    <div class="card border-primary">
-                        <img class="card-img-top" src="<?php echo 'painel-cliente/img_animais/' . $dados['url_imagem']; ?>"
-                            alt="Imagem do animal">
-
-                        <div class="card-body">
-                            <h5 class="card-title">
-                                <?php echo $dados['nm_animal']; ?>
-                            </h5>
-                            <p class="card-text">
-                                <?php echo $dados['descricao']; ?>
-                            </p>
-                            <a href="#" class="btn btn-primary">Visitar</a>
-                        </div>
-                    </div>
-                </div>
-
-
-                <?php
-            }
-            ?>
-        </div>
-    </div>
-
 </body>
