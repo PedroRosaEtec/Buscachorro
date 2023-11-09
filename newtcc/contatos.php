4fe6b3c30fa03d4a25fd1a74373807c4614adf16
PedroRosaEtec <133720869+PedroRosaEtec@users.noreply.github.com>
2023-11-09 20:37:42 -0300

    css apagado
    desnecessario

--- newtcc/contatos.php
+++ newtcc/contatos.php
@@ -7,27 +7,43 @@ require_once "navigation.php";
 
 <body>
 
-<div class="centered-card">
-    <div class="card" style="position: relative; top: -20px; box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);">
-        <div class="card-content">
-            <div class="media">
-                <div class="media-left">
-                    <figure class="image is-48x48">
-                        <img src="./img/logo.png" alt="Placeholder image" width="200" height="300">
-                    </figure>
-                </div>
-                <div class="media-content">
-                    <p class="title is-4" style="font-family: 'Nome da Fonte Desejada'; font-size: 50px;">Buscachorro</p>
-                    <p class="title is-4">Buscachorro é um serviço online que se dedica à busca e auxílio na localização de cães perdidos. </p>
-                    <p class="title is-4">Confira nossas Redes.</p>
-                    <a class="btn" style="background-color: transparent; border: 2px solid #007bff; color: #007bff; padding: 10px 20px; text-decoration: none; margin: 5px; border-radius: 5px;" href="https://www.instagram.com/buscachorro/">Instagram</a>
-                    <a class="btn" style="background-color: transparent; border: 2px solid #007bff; color: #007bff; padding: 10px 20px; text-decoration: none; margin: 5px; border-radius: 5px;" href="https://api.whatsapp.com/send?phone=12991638739">WhatsApp</a>
-                </div>
-            </div>
-        </div>
+<div class="container" Style= "margin-top:10%">
+<div class="row">
+  <div class="col-sm-4 offset-sm-2">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Instagram</h5>
+        <p class="card-text">acompanhe o projeto buscachorro pelo instagram</p>
+        <a href="https://www.instagram.com/buscachorro/" class="btn btn-primary">Instagram</a>
+      </div>
     </div>
+  </div>
+  <div class="col-sm-4 ">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Whatsapp</h5>
+        <p class="card-text">Converse com o suporte pelo whatsapp</p>
+        <a href="https://api.whatsapp.com/send?phone=12991638739" class="btn btn-primary">Whatsapp</a>
+      </div>
+      </div>
+    </div>
+  </div>
 </div>
 
 
 
 </body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- newtcc/sobre.php
+++ newtcc/sobre.php
@@ -7,7 +7,7 @@ require_once "navigation.php";
 
 <body>
 
-<div class="card" style="width: 18rem;">
+<div class="card" style= "width : 35rem; margin-left:30%">
   <img class="card-img-top" src="./img/sobre.jpg" alt="Imagem de capa do card">
   <div class="card-body">
     <p class="card-text">Cada minuto conta quando um animal está desaparecido, e os riscos que eles enfrentam são significativos. Portanto, serviços como "Buscachorro" desempenham um papel crucial ao fornecer uma plataforma e recursos para ajudar os proprietários a reunir-se com seus animais de estimação perdidos.</p>
