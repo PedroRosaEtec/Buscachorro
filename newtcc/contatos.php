a2e6b4df073797426b0aa9c14747335126e00ee7
PedroRosaEtec <133720869+PedroRosaEtec@users.noreply.github.com>
2023-11-11 11:26:39 -0300

    Revert "novamente arrumando bugs"
    This reverts commit b3e4dc05a83d909ee29ea07474bd620539ee9531.

--- newtcc/contatos.php
+++ newtcc/contatos.php
@@ -7,46 +7,27 @@ require_once "navigation.php";
 
 <body>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <!-- Add Bootstrap CSS link here -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Your Title</title>
-  <!-- Add your custom styles or additional CSS links here -->
-</head>
-<body>
-
-<div class="container text-center mt-5"> <!-- Add 'text-center' class for centering -->
-  <div class="row">
-    <div class="col-sm-6 mb-3 mb-sm-0">
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">Instagram</h5>
-          <p class="card-text">Acompanhe o Buscachorro no Instagram</p>
-          <a href="#" class="btn btn-primary">Instagram</a>
+<div class="centered-card">
+    <div class="card" style="position: relative; top: -20px; box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);">
+        <div class="card-content">
+            <div class="media">
+                <div class="media-left">
+                    <figure class="image is-48x48">
+                        <img src="./img/logo.png" alt="Placeholder image" width="200" height="300">
+                    </figure>
+                </div>
+                <div class="media-content">
+                    <p class="title is-4" style="font-family: 'Nome da Fonte Desejada'; font-size: 50px;">Buscachorro</p>
+                    <p class="title is-4">Buscachorro é um serviço online que se dedica à busca e auxílio na localização de cães perdidos. </p>
+                    <p class="title is-4">Confira nossas Redes.</p>
+                    <a class="btn" style="background-color: transparent; border: 2px solid #007bff; color: #007bff; padding: 10px 20px; text-decoration: none; margin: 5px; border-radius: 5px;" href="https://www.instagram.com/buscachorro/">Instagram</a>
+                    <a class="btn" style="background-color: transparent; border: 2px solid #007bff; color: #007bff; padding: 10px 20px; text-decoration: none; margin: 5px; border-radius: 5px;" href="https://api.whatsapp.com/send?phone=12991638739">WhatsApp</a>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-    <div class="col-sm-6">
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">Whatsapp</h5>
-          <p class="card-text">Fale com o suporte do Buscachorro</p>
-          <a href="#" class="btn btn-primary">Whatsapp</a>
-        </div>
-      </div>
-    </div>
-  </div>
 </div>
 
-<!-- Add Bootstrap JS and any additional scripts here -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 
 </body>
-
-
