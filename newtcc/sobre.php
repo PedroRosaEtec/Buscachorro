30ca14a3d02f0c9cce0e7a32265bf7add8fc467f
PedroRosaEtec <133720869+PedroRosaEtec@users.noreply.github.com>
2023-11-14 19:43:20 -0300

    responsivo dnv
    ...

--- newtcc/sobre.php
+++ newtcc/sobre.php
@@ -7,12 +7,17 @@ require_once "navigation.php";
 
 <body>
 
-<div class="card" style= "width : 35rem; margin-left:30%">
+<div class="container" Style= "margin-top:5%">
+<div class="row">
+  <div class="col-sm-8 offset-sm-2">
   <img class="card-img-top" src="./img/sobre.jpg" alt="Imagem de capa do card">
-  <div class="card-body">
-    <p class="card-text">Cada minuto conta quando um animal está desaparecido, e os riscos que eles enfrentam são significativos. Portanto, serviços como "Buscachorro" desempenham um papel crucial ao fornecer uma plataforma e recursos para ajudar os proprietários a reunir-se com seus animais de estimação perdidos.</p>
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Buscachorro</h5>
+        <p class="card-text">Cada minuto conta quando um animal está desaparecido, e os riscos que eles enfrentam são significativos. Portanto, serviços como "Buscachorro" desempenham um papel crucial ao fornecer uma plataforma e recursos para ajudar os proprietários a reunir-se com seus animais de estimação perdidos.</p>
+      </div>
+    </div>
   </div>
-</div>
 
 </body>
 
