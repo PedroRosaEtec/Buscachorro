87c2f7a7ea7494ae3a86f230f8422b605df0b401
PicoleDeLimao <145725289+PicoleDeLimao@users.noreply.github.com>
2023-11-14 21:06:54 -0300

    Site principal upado pra net

---
+++ painel-adm/dados_legais.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'header.php';
+require_once 'lev_raca.php';
+require_once 'lev_cor.php';
+require_once 'navigation.php';
+?>
+<head>
+  
+</head>
+
+<body>
+  <div class="container">
+    <div class="row center">
+
+      <div id="lev_raca" style="width: 900px; height: 500px;"></div>
+      <div id="lev_cor" style="width: 900px; height: 500px;"></div>
+
+    </div>
+
+  </div>
+</body>
