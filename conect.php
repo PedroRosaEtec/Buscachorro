9c75f256b3cb25790d46d2516288e0f13e5ac03d
PicoleDeLimao <145725289+PicoleDeLimao@users.noreply.github.com>
2023-11-16 22:14:45 -0300

    PHP mailer adicionado para teste

--- conect.php
+++ conect.php
@@ -5,6 +5,11 @@ $user = "buscachorro";
 $pass = "buscachorroTCC2023!";
 $database = "buscachorro";
 
+//$host = "localhost";
+//$user = "root";
+//$pass = "";
+//$database = "db_buscachorro";
+
 $con = new mysqli($host, $user, $pass, $database);
 
 ?>
